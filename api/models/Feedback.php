018490078a7be4b16bc88d5b2548dacd10ec1cd2
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-07-22 21:33:24 +0300

    send email

--- api/models/Feedback.php
+++ api/models/Feedback.php
@@ -5,12 +5,14 @@ namespace api\models;
 use yii\helpers\Url;
 use yii\web\Linkable;
 use yii\web\Link;
+use Yii;
 
 /**
  * @author Eugene Terentev <eugene@terentev.net>
  */
 class Feedback extends \common\models\Feedback implements Linkable
 {
+
     public function fields()
     {
         return ['id', 'nick', 'email', 'message', 'lang'];
@@ -31,5 +33,26 @@ class Feedback extends \common\models\Feedback implements Linkable
         return [
             Link::REL_SELF => Url::to(['feedback/view', 'id' => $this->id], true)
         ];
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        $this->sendEmails();
+
+        return parent::afterSave($insert, $changedAttributes);
+    }
+    
+    private function sendEmails()
+    {
+        mail('eugene.fabrikov@gmail.com','subj','message');
+
+        $emails  = Yii::$app->keyStorage->get('frontend_feedback_form_emals');
+        $emails  = explode(',', $emails);
+        $subject = 'Feedback request from ' . Yii::$app->request->get('nick');
+        $message = Yii::$app->request->get('email') . '<br>' . Yii::$app->request->get('message');
+
+        foreach ($emails as $value) {
+            mail($value, $subject, $message);
+        }
     }
 }
