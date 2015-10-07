96020b3354860b30c522e8b58d503c9d7c0b7ac3
Jar-L <yaros.lin@gmail.com>
2015-10-08 00:07:18 +0300

    deprecated browser page

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -34,11 +34,15 @@ use yii\web\Response;
 		
 			var ng_userAgent=navigator.userAgent,
 				ng_appVersion=parseInt(navigator.appVersion),
-				ng_platform=navigator.platform;
+				ng_platform=navigator.platform,
+				br_name=null,
+				br_version=null;
 				
 			/*alert(ng_userAgent);
 			alert(ng_appVersion);
 			alert(ng_platform);*/
+			
+			
 		
 			/*if((ng_platform.indexOf('Win')>-1&&ng_userAgent.indexOf('Safari')>-1)||
 				(ng_userAgent.indexOf('Safari')>-1&&ng_appVersion<8)||
@@ -47,13 +51,48 @@ use yii\web\Response;
 				(ng_userAgent.indexOf('MSIE')>-1&&ng_appVersion<11))
 				location.href="/page/newbrowser.html";*/
 				
-			console.log(ng_userAgent);
+				
+				
+			/*console.log(ng_userAgent);
 			console.log(ng_appVersion);
-			console.log(ng_platform);
+			console.log(ng_platform);*/
 			
 			/*if(!Modernizr.cssanimations||!Modernizr.backgroundsize||!Modernizr.boxsizing||!Modernizr.csstransforms||!Modernizr.csstransitions){
 				location.href="/page/newbrowser.html";
 			}*/
+			
+			
+			
+			var ua= navigator.userAgent, tem, 
+			M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+			if(/trident/i.test(M[1])){
+				tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+				//return 'IE '+(tem[1] || '');
+				br_name='MSIE';
+				br_version=tem[1];
+			}
+			else if(M[1]=== 'Chrome'){
+				tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+				if(tem!= null){
+					//return tem.slice(1).join(' ').replace('OPR', 'Opera');
+					br_name='Opera';
+					br_version=tem[21];
+				}
+			}
+			else{
+				M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+				if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+				
+				br_name=M[1];
+				br_version=M[2];
+			}
+			
+			if( (br_name=='Safari'&&br_version<8)||
+				(br_name=='Opera'&&br_version<25)||
+				(br_name=='Firefox'&&br_version<25)||
+				(br_name=='MSIE'&&br_version<11))
+				location.href="/page/newbrowser.html";
+			
 
 		</script>
 		
--- frontend/web/js/lib/afterRender.js
+++ frontend/web/js/lib/afterRender.js
@@ -10,15 +10,7 @@ $(document).ready(function () {
 
 	function heightFix(){
 		$('.main_top').css({'height':$(window).height()});
-		//console.log($(window).height());
+		console.log($(window).height());
 	}
-	
-	
-	var ng_userAgent=navigator.userAgent,
-		ng_appVersion=parseInt(navigator.appVersion),
-		ng_platform=navigator.platform;
-	console.log(ng_userAgent);
-	console.log(ng_appVersion);
-	console.log(ng_platform);
 	
 });
