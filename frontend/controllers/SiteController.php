b83afd07c62600dc14679a5b8f508919cf2f5a9c
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2015-10-12 09:30:24 +0300

    Заглушка для старих браузерів

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -18,34 +18,35 @@ class SiteController extends Controller
     public function actions()
     {
         return [
-            'error'      => [
+            'error'   => [
                 'class' => 'yii\web\ErrorAction'
             ],
-            'captcha'    => [
+            'captcha' => [
                 'class'           => 'yii\captcha\CaptchaAction',
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null
             ],
-            /*'set-locale' => [
-                'class'   => 'common\actions\SetLocaleAction',
-                'locales' => array_keys(Yii::$app->params['availableLocales'])
-            ]*/
+            /* 'set-locale' => [
+              'class'   => 'common\actions\SetLocaleAction',
+              'locales' => array_keys(Yii::$app->params['availableLocales'])
+              ] */
         ];
     }
 
     public function actionIndex()
-    {
-        //$verbs        
+    {        
+        $this->_checkBrowser();
+
         $this->layout = '@frontend/views/layouts/main.php';
         return $this->render('index');
     }
 
     public function actionMessage($message)
     {
-        return $this->render('message',[
-            'message' => $message,
+        return $this->render('message', [
+                'message' => $message,
         ]);
     }
-    
+
     public function actionFeedback()
     {
         var_dump($_POST);
@@ -53,9 +54,19 @@ class SiteController extends Controller
 
     public function beforeAction($action)
     {
-        if ($action->id == 'error')
-            $this->layout = 'main.php';
+        if ($action->id == 'error') $this->layout = 'main.php';
 
         return parent::beforeAction($action);
+    }
+
+    private function _checkBrowser()
+    {
+        $badBrowsers = file(Yii::getAlias('@frontend/config/browserList.txt'));
+
+        //\yii\helpers\VarDumper::dump($badBrowsers, 11, 1);
+        //echo $_SERVER['HTTP_USER_AGENT'];
+        if (in_array($_SERVER['HTTP_USER_AGENT'], $badBrowsers)) {
+            $this->redirect(Yii::getAlias('@frontendUrl/page/newbrowser.html'));
+        }
     }
 }
